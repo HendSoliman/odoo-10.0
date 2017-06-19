da80a6b588abbe86ba717cbcba76ed3e51fed76b
solimahg <eng_hendsoliman@yahoo.com>
2017-06-19 13:47:05 +0200

    [IMP]

--- addons/advansys/__init__.py
+++ addons/advansys/__init__.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
-
+# 1 : imports of python lib
+#======= from datetime import datetime
+# 2 :  imports of odoo
+# import odoo
+#====== from odoo import api, fields, models # alphabetically ordered
+# 3 :  imports from odoo modules
+#===== from odoo.addons.website.models.website import slugt
 from . import controllers
 from . import models
 from . import report
--- addons/advansys/models/advansys_employee.py
+++ addons/advansys/models/advansys_employee.py
@@ -11,19 +11,51 @@ from odoo import tools, _
 from odoo.exceptions import ValidationError
 from odoo.modules.module import get_module_resource
 
+
 class AdvansysEmployee(models.Model):
+    # private attributes
     _name = 'advansys.employee'
 
-    @api.depends('gender')
-    def _computed_salary(self):
-        for record in self:
-            if self.gender == 'm':
-                self.salary = 5000
-            else:
-                self.salary = 8000
+    # Default Methods
+    @api.model
+    def _default_image(self):
+        image_path = get_module_resource('advansys', 'static/src/img', 'default_image.png')
+        return tools.image_resize_image_big(open(image_path, 'rb').read().encode('base64'))
 
-                # To compute Age Automatically
+        # Fields declaration
+        name = fields.Char(string="Employee Name", required=True)
+        salary = fields.Float(compute=_computed_salary)
+        age = fields.Char(compute=_computed_age, readonly=True)
+        email = fields.Char()
+        title = fields.Char()
+        gender = fields.Selection([
+            ('m', 'Male'),
+            ('f', 'Female'),
+        ], default='m')
+        join_data = fields.Date(required=True)
+        date_of_birth = fields.Date(default=fields.Date.context_today)
+        is_accepted = fields.Boolean()
+        bio = fields.Html()
+        state = fields.Selection([
+            ('apply', 'Applied'),
+            ('iq', 'Passed IQ'),
+            ('tech', 'Passed Technical'),
+            ('rejected', 'rejected'),
+            ('accepted', 'Accepted'),
+        ], default='apply')
+        military_status = fields.Char()
+        phone = fields.Char()
+        address = fields.Char()
 
+        # Relations
+        departmen_id = fields.Many2one('advansys.department')
+        departmen_desc = fields.Text(related='departmen_id.desc', store=True, readonly=True)
+
+        project_ids = fields.Many2many('advansys.project')
+        employee_skill_level_ids = fields.One2many('advansys.employee.skill.level', 'employee_id')
+
+    # compute and search fields
+    # To compute Age Automatically
     @api.depends('date_of_birth')
     def _computed_age(self):
         for rec in self:
@@ -34,6 +66,15 @@ class AdvansysEmployee(models.Model):
         rd = relativedelta(d2, d1)
         rec.age = str(rd.years) + ' years\t' + str(rd.months) + 'months'
 
+    @api.depends('gender')
+    def _computed_salary(self):
+        for record in self:
+            if self.gender == 'm':
+                self.salary = 5000
+            else:
+                self.salary = 8000
+
+    # Constraints and onchanges
     @api.onchange('gender')
     def onChange_gender(self):
         print 'On Onchage method'
@@ -44,41 +85,14 @@ class AdvansysEmployee(models.Model):
             'domain': {'departmen_id': domain}
         }
 
-    name = fields.Char(string="Employee Name", required=True)
-    salary = fields.Float(compute=_computed_salary)
-    age = fields.Char(compute=_computed_age, readonly=True)
-    email = fields.Char()
-    title = fields.Char()
-    gender = fields.Selection([
-        ('m', 'Male'),
-        ('f', 'Female'),
-    ], default='m')
-    # desc = fields.Text(default='Default Description')
-    join_data = fields.Date(required=True)
-    date_of_birth = fields.Date(default=fields.Date.context_today)
-    is_accepted = fields.Boolean()
-    bio = fields.Html()
-    state = fields.Selection([
-        ('apply', 'Applied'),
-        ('iq', 'Passed IQ'),
-        ('tech', 'Passed Technical'),
-        ('rejected', 'rejected'),
-        ('accepted', 'Accepted'),
-    ], default='apply')
-    military_status = fields.Char()
-    phone=fields.Char()
-    address=fields.Char()
+    # CRUD methods (and name_get, name_search, ...) overrides
+    @api.model
+    def create(self, vals):
+        tools.image_resize_images(vals)
+        return super(AdvansysEmployee, self).create(vals)
 
-    # Relations btw Employee & Department
-    # ================Many2one
-    departmen_id = fields.Many2one('advansys.department')
-    departmen_desc = fields.Text(related='departmen_id.desc', store=True, readonly=True)
-
-    # Relation btw Employee & project
-    # ==================Many2many
-    project_ids = fields.Many2many('advansys.project')
-    employee_skill_level_ids = fields.One2many('advansys.employee.skill.level', 'employee_id')
-
+    # Action methods
+    # Business methods
     # context is an object used in environment and we can access its var during code
     """"def change_state(self):
         new_state=self.env.context['state']
@@ -88,24 +102,14 @@ class AdvansysEmployee(models.Model):
         new_state = self.env.context['state']
         self.write({'state': new_state})
 
-    @api.model
-    def _default_image(self):
-        image_path = get_module_resource('advansys', 'static/src/img', 'default_image.png')
-        return tools.image_resize_image_big(open(image_path, 'rb').read().encode('base64'))
-
-    @api.model
-    def create(self, vals):
-        tools.image_resize_images(vals)
-        return super(AdvansysEmployee, self).create(vals)
-
     # image: all image fields are base64 encoded and PIL-supported
     image = fields.Binary("Photo", attachment=True,
-        help="This field holds the image used as photo for the employee, limited to 1024x1024px.")
+                          help="This field holds the image used as photo for the employee, limited to 1024x1024px.")
     image_medium = fields.Binary("Medium-sized photo", attachment=True,
-        help="Medium-sized photo of the employee. It is automatically "
-             "resized as a 128x128px image, with aspect ratio preserved. "
-             "Use this field in form views or some kanban views.")
+                                 help="Medium-sized photo of the employee. It is automatically "
+                                      "resized as a 128x128px image, with aspect ratio preserved. "
+                                      "Use this field in form views or some kanban views.")
     image_small = fields.Binary("Small-sized photo", attachment=True,
-        help="Small-sized photo of the employee. It is automatically "
-             "resized as a 64x64px image, with aspect ratio preserved. "
-             "Use this field anywhere a small image is required.")
+                                help="Small-sized photo of the employee. It is automatically "
+                                     "resized as a 64x64px image, with aspect ratio preserved. "
+                                     "Use this field anywhere a small image is required.")
